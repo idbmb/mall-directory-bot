4d873428510c9bfb6a8b4241e11b1e9201b66e59
Diky Arga <dikyarga.id@gmail.com>
2017-03-24 12:25:58 +0800

    Create webhook

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ var router = express.Router();
 
 let queris = require('../helper/queris')
 
+var apiai = require('apiai');
+var app = apiai("391e94e2d8634c8f8316b11a1360243a");
 
 router.get('/', function(req, res, next) {
   res.json({
@@ -11,11 +13,32 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/webhook', function(req, res, next) {
-  queris.storeAtMall(req.body.store, req.body.mall).then(function(data){
-    res.json({
-      result: data
-    })
-  });
+  console.log('request : ', req.body.result.action);
+  if (req.body.result.action == 'cari_toko_di_mall') {
+    console.log('running', req.body.result.parameters);
+    queris.storeAtMall(req.body.result.parameters.toko, req.body.result.parameters.mall).then(function(data){
+      console.log('hasil : ', data);
+      res.json({
+        speech: data,
+        displayText: data
+      })
+    });
+  }
+// var request = app.textRequest('bca di aeon', {
+//     sessionId: '<unique session id>'
+// });
+//
+// request.on('response', function(response) {
+//     console.log(response);
+// });
+//
+// request.on('error', function(error) {
+//     console.log(error);
+// });
+//
+// request.end();
+
+
 });
 
 module.exports = router;
